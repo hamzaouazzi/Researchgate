7809aea32445e201de172ddb2363acb634cb73e6
HAMZA OUAZZI <ouazzihamza97@gmail.com>
2020-11-05 12:03:21 +0100

    first commit

---
+++ src/main/java/ma/ensa/researchgate/ResearchgateApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.researchgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchgateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchgateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/ensa/researchgate/ResearchgateApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.researchgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchgateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
